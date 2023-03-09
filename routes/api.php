b88ac25a035a7cbdc9b56808882518fa06c01df9
Mark-dev30 <fronterremarco@gmail.com>
2023-03-09 15:20:44 +0100

    Milestone 2 Completed

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('projects', [ProjectController::class, 'index']);
+Route::get('/projects', [ProjectController::class, 'index']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
