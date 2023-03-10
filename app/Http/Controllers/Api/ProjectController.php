8417991ffba20c7b66e5e914352599912b6f6c6d
Mark-dev30 <fronterremarco@gmail.com>
2023-03-10 16:25:12 +0100

    route show added

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,11 +10,28 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::all()/* with('type', 'technologies')->paginate(10) */;
 
         return response()->json([
             'success' => true,
             'projects' => $projects
         ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::with('types', 'technologies')->where('slug', $slug)->first();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'post' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Nessun Progetto Trovato'
+            ]);
+        }
     }
 }
